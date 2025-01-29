82de5c62c66890a4ce2ff74b398d0e2c53765836
ObaidAlhmoudi59726 <73142420+Alhammoudi@users.noreply.github.com>
2025-01-29 21:50:45 +0400

    Update move_semantics3.rs

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -1,5 +1,5 @@
 // TODO: Fix the compiler error in the function without adding any new line.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
@@ -7,6 +7,9 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
 fn main() {
     // You can optionally experiment here.
+    let vectest = vec![10, 20, 33, 64];
+    println!("Original vec = {:?}", vectest);
+    fill_vec(vectest);
 }
 
 #[cfg(test)]
